4fc8ea5d168f5ed9ffc3db7276916f1cf86281a0
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2022-10-03 17:24:30 +0530

    Added Solution for Paradise using Java

---
+++ Geek-Start-Editorials/CSE-QUE/Paradise/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        n = input.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i < n;i++){
+            a[i] = input.nextInt();
+        }
+        int count=0;
+        for(int i = 0; i < n;i++){
+            if(a[i]!=0)
+                System.out.print(a[i]+" ");
+            else
+                count++;
+        }
+        for(int i = 0;i < count ;i++){
+            System.out.print(0+" ");
+        }
+    }
+}

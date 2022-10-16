6e69b46d95f6ab3356d6c10e0c5ce6a7d8ce896b
GFGSCLIET <114823075+GFGSCLIET@users.noreply.github.com>
2022-10-16 17:02:55 +0530

    Create Solution.py

---
+++ Geek-Start-Editorials/CSE-QUE/Trade Now/Solution.py
@@ -0,0 +1,10 @@
+n = int(input()) 
+l = list(map(int,input().split())) 
+p = 0 
+ 
+for i in range(n): 
+    for j in range(i+1,n): 
+        if l[j]>l[i]: 
+            p = max(l[j] - l[i],p) 
+ 
+print(p)

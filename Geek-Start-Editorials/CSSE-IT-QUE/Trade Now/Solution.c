e3c1e024ab64c51c5e553cb7adfadc9db201e189
GFGSCLIET <114823075+GFGSCLIET@users.noreply.github.com>
2022-10-16 16:13:43 +0530

    Create Solution.c

---
+++ Geek-Start-Editorials/CSSE-IT-QUE/Trade Now/Solution.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main() {
+    
+    int n,i,j,p=0;
+    scanf("%d",&n);
+    
+    int arr[n];
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        for(j= i+1;j<n;j++)
+        {
+            if (arr[j]>arr[i])
+            {
+                if (arr[j]-arr[i] > p)
+                    p = arr[j]-arr[i];
+                    
+            }
+        }
+    }
+    printf("%d",p);

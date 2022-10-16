67e51d3fa770cca162fcdec09f11776a6a8c0742
GFGSCLIET <114823075+GFGSCLIET@users.noreply.github.com>
2022-10-16 19:38:05 +0530

    Create Solution.java

---
+++ Geek-Start-Editorials/CSE-QUE/SubString_Count/Solution.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    static long substrCount (String s, int k) {
+      return solve(s,k)-solve(s,k-1);
+    }
+    
+    static long solve(String s, int k) {
+        int i=0,j=0,size=0,n=s.length();
+        long ans=0;
+        int[] map = new int[26];
+        while(j<n) {
+            char cha = s.charAt(j);
+            if(map[cha-'a']==0) size++;
+            map[cha-'a']++;
+            while(size>k) {
+                char chr = s.charAt(i);
+                map[chr-'a']--;
+                if(map[chr-'a']==0) size--;
+                i++;
+            }
+            ans+=j-i+1;
+            j++;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.next();
+        int n = sc.nextInt();
+        
+        System.out.println(substrCount(s,n));
+        
+    }
+}
